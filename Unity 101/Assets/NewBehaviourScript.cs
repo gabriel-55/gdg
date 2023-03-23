2a29d8edf524ba1d7fd3a6e33f92fa82982e6d3b
Gabriel <renokoku@gmail.com>
2023-03-24 08:42:55 +1300

    no message

---
+++ Unity 101/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int q = 30;
+        print(q);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
